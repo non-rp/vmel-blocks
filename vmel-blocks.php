ab6c7883f2aae0f0b8705129ef5daad540b7e602
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-02-21 22:25:14 +0200

    Add initial plugin files for VMEL Blocks with metadata

---
+++ vmel-blocks.php
@@ -0,0 +1,14 @@
+/*
+* Plugin Name: VMEL Blocks
+* Description: A collection of custom Gutenberg blocks for WordPress.
+* Version: 1.0.0
+* Requires at least: 8.0
+* Requires PHP: 8.0
+* Author: Valentyn Melnychenko
+* Author URI: https://v-mel.dev
+* License: GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: vmel-blocks
+* Domain Path: /languages
+* Requires Plugins: gutenberg
+*/
