8446dea857c9de6794430973e98ec8ccca8d6b6d
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-02-22 23:39:10 +0200

    Fix minimum required WordPress version in plugin header

--- vmel-blocks.php
+++ vmel-blocks.php
@@ -2,7 +2,7 @@
 * Plugin Name: VMEL Blocks
 * Description: A collection of custom Gutenberg blocks for WordPress.
 * Version: 1.0.0
-* Requires at least: 8.0
+* Requires at least: 5.0
 * Requires PHP: 8.0
 * Author: Valentyn Melnychenko
 * Author URI: https://v-mel.dev
@@ -10,5 +10,4 @@
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
 * Text Domain: vmel-blocks
 * Domain Path: /languages
-* Requires Plugins: gutenberg
 */
